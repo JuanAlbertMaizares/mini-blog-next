206b451ecaa0f116ec63da25be8e41ee1d6eca47
Maizares, Juan <maizaresjuan.coder@gmail.com>
2023-02-18 15:29:45 -0300

    componente ssr que expone data

--- mini-blog/app/posts/page.jsx
+++ mini-blog/app/posts/page.jsx
@@ -1,5 +1,18 @@
-export default function Post () {
+const fetchPosts = () => {
+  return fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+}
+
+export default async function PostsPage ({params}) {
+    const posts = await fetchPosts()
     return (
-      <h1>Post</h1>
-    )
+        <section>
+            {posts.map(post => (
+                <article key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </article>
+            ))}
+        </section>
+        )
   }
