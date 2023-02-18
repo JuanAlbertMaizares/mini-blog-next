e8b1eb4a3298be34325bdedbdbed6ea4585c5064
Maizares, Juan <maizaresjuan.coder@gmail.com>
2023-02-18 14:53:24 -0300

    agregando estilos a nivel componente

--- mini-blog/app/components/Navigation.jsx
+++ mini-blog/app/components/Navigation.jsx
@@ -1,3 +1,4 @@
+import styles from './Navigation.module.css';
 import Link from 'next/link'
 
 const links = [{
@@ -10,9 +11,9 @@ const links = [{
 
 export function Navigation () {
   return (
-    <header>
+    <header className={styles.header}>
       <nav>
-        <ul>
+        <ul className={styles.navigation}>
           {links.map(({ label, route }) => (
             <li key={route}>
               <Link href={route}>
